6f62170936e46ab82bbe49c5ae08039d5b3a0e18
figo5530 <figooooo0221@gmail.com>
2021-04-29 21:01:56 +0800

    commit tree ef15a0d366e7ef62936599cdbe1807e15d769cf8

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function mapToNegativize(arr) {
+    return arr.map((i) => -i)
+}
+
+function mapToNoChange(arr) {
+    return arr.map((i) => i)
+}
+
+function mapToDouble(arr) {
+    return arr.map((i) => i*2 )
+}
+
+function mapToSquare(arr) {
+    return arr.map((i) => i*i)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    return sourceArray.reduce((a,c) => a + c, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!!sourceArray[i]) return true
+    }
+    return false
+}
